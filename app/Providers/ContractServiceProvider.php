8d52a169b5abe2b7111740aabd5ea1ca4a7ccc0d
ilzam <ilzammulkhaq85@gmail.com>
2024-08-08 00:47:22 +0700

    feat: create basic file

---
+++ app/Providers/ContractServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Providers;
+
+use App\Contract\BaseContract;
+use App\Service\BaseService;
+use Illuminate\Support\ServiceProvider;
+
+class ContractServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        $this->app->bind(BaseContract::class, BaseService::class);
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,4 +2,6 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    App\Providers\ContractServiceProvider::class,
+    Satusehat\Integration\SatusehatIntegrationServiceProvider::class,
 ];
