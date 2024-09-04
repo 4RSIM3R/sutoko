b640639f99c357b144c68441817a09892032f96d
ilzam <ilzammulkhaq85@gmail.com>
2024-09-04 21:46:12 +0700

    feat: begin to develop treatment, and medicine

--- database/migrations/2024_08_07_173916_create_settings_table.php
+++ database/migrations/2024_08_07_173916_create_settings_table.php
@@ -13,6 +13,17 @@ return new class extends Migration
     {
         Schema::create('settings', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->enum('mode', ['dev', 'prod'])->default('dev');
+            $table->string('satset_client_id');
+            $table->string('satset_client_secret');
+            $table->string('satset_org_id');
+            $table->string('bpjs_cons_id')->nullable();
+            $table->string('bpjs_secret_key')->nullable();
+            $table->string('bpjs_user_key')->nullable();
+            $table->string('bpjs_username')->nullable();
+            $table->string('bpjs_password')->nullable();
             $table->timestamps();
         });
     }
